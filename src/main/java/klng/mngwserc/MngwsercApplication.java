dadd26ef4da54aa44cb016fdcdf760e1c9638284
hyumain <hyumain@easymedia.net>
2021-12-13 18:26:14 +0900

    최초 생성

---
+++ src/main/java/klng/mngwserc/MngwsercApplication.java
@@ -0,0 +1,13 @@
+package klng.mngwserc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MngwsercApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MngwsercApplication.class, args);
+	}
+
+}
---
+++ src/test/java/klng/mngwserc/MngwsercApplicationTests.java
@@ -0,0 +1,13 @@
+package klng.mngwserc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MngwsercApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
